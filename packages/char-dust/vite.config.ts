19c3382339d98796c565abdf484caa966efac07b
YunYouJun <me@yunyoujun.cn>
2021-03-11 23:25:39 +0800

    :green_heart: fix vite build:lib

--- packages/char-dust/vite.config.ts
+++ packages/char-dust/vite.config.ts
@@ -6,7 +6,7 @@ import pkg from "./package.json";
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "index.ts"),
+      entry: path.resolve(__dirname, "src/index.ts"),
       name: pkg.name,
     },
   },
